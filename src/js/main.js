c1bcb9ca80d389b558d537e39fb019880d2ed9e8
rian <rianrgomes@gmail.com>
2024-09-07 15:01:41 -0300

    base commit

---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+const targets = Array.from(document.querySelectorAll('[data-scroll]'));
+const span = Array.from(document.querySelector('main ul span'));
+
+
+const revealScroll = () =>{
+  const windowTop = window.pageYOffset + window.innerHeight*0.6;
+
+  targets.forEach(target =>{
+    const span = target.querySelector('span');
+
+    if(windowTop>target.offsetTop){
+      target.classList.add('selected');
+      span.classList.add('span');
+    } else{
+      target.classList.remove('selected');
+      span.classList.remove('span');
+    }
+  })
+}
+revealScroll();
+if(targets.length){
+  window.addEventListener('scroll', () =>{
+    revealScroll();
+  })
+}
