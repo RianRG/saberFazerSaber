a4ec874436f56479dd0b80a10e10cf70ce74d24b
rian <rianrgomes@gmail.com>
2024-09-08 10:22:30 -0300

    feat: form background styles+

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ const targets = Array.from(document.querySelectorAll('[data-scroll]'));
 const span = Array.from(document.querySelectorAll('main ul span'));
 const button = document.querySelector('aside form button');
 const overAll = document.querySelector('.overall');
+const questions = Array.from(document.querySelectorAll('.question'));
 button.addEventListener('click', (e) =>{
   e.preventDefault();
   overAll.classList.add('active');
@@ -11,7 +12,15 @@ button.addEventListener('click', (e) =>{
 
 const revealScroll = () =>{
   const windowTop = window.pageYOffset + window.innerHeight * 0.2;
-  
+  if(windowTop >= 1600){
+    document.body.style.backgroundColor='#4e2727'
+    questions.forEach(question => question.classList.add('shown'))
+  }
+  else{
+    document.body.style.backgroundColor='#91e3ee'
+    questions.forEach(question => question.classList.remove('shown'))
+  }
+
   targets.forEach(target =>{
     const span = target.querySelector('span');
 
