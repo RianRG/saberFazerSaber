4a3489ec5005ca4d753a9a5686d36772c91793a9
RianRG <rianrgomes@gmail.com>
2024-11-05 20:19:24 -0300

    feat: add a quiz

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,17 @@ const span = Array.from(document.querySelectorAll('main ul span'));
 const button = document.querySelector('aside form button');
 const overAll = document.querySelector('.overall');
 const questions = Array.from(document.querySelectorAll('.question'));
+const form = document.querySelector('.password');
+const input = document.querySelector('input');
+const onCapitalLetter = document.querySelector('.onCapitalLetter');
+
+form.addEventListener('submit', (e) =>{
+  e.preventDefault();
+  if(input.value === 'Lins') onCapitalLetter.classList.add('active')
+  else if(input.value==='lins')
+    window.location.href='https://youtube.com/shorts/rgS_v13tIFI?feature=share'
+})
+
 button.addEventListener('click', (e) =>{
   e.preventDefault();
   overAll.classList.add('active');
@@ -12,11 +23,12 @@ button.addEventListener('click', (e) =>{
 
 const revealScroll = () =>{
   const windowTop = window.pageYOffset + window.innerHeight * 0.2;
-  if(windowTop >= 1500){
+  if(windowTop >= 1500 && windowTop<=2000){
     document.body.style.backgroundColor='#4e2727'
     questions.forEach(question => question.classList.add('shown'))
-  }
-  else{
+  } else if(windowTop>=2000){
+    document.body.style.backgroundColor='#317e9c'
+  } else{
     document.body.style.backgroundColor='#91e3ee'
     questions.forEach(question => question.classList.remove('shown'))
   }
