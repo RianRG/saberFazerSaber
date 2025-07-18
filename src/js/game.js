ba24a7f6e4a90851b32d723887b9ed034750ec96
RianRG <rianrgomes@gmail.com>
2025-07-18 19:20:26 -0300

    feat: set dialogues box

--- src/js/game.js
+++ src/js/game.js
@@ -2,6 +2,11 @@ const popup = document.querySelector('main aside.shown.first');
 const question = document.querySelector('main aside.shown.question')
 const form = question.querySelector('form')
 const lett = document.querySelector('main img.let');
+const textDialogue = document.querySelector('main .dialogueDiv .dialogue .textDialogue')
+const character = document.querySelector('main .dialogueDiv .dialogue img')
+const username = textDialogue.querySelector('h4');
+const skipButton = textDialogue.querySelector('span');
+const paragraph = textDialogue.querySelector('p')
 popup.classList.remove('shown');
 
 const swordAudio = new Audio('./src/imgs/swordAudio.mp3');
@@ -50,4 +55,80 @@ form.addEventListener('submit', (e) =>{
   setTimeout(() =>{
     lett.src= idleGif + '?t=' + new Date().getTime();
   }, 4000)
+})
+
+//=========================================== dialogues
+
+const writingAnimation = async (textHtml) =>{
+  skipButton.style.pointerEvents='none';
+  let text=textHtml.textContent;
+  textHtml.textContent='';
+  const promise = new Promise((res, rej) =>{
+    for (let k=0; k<text.length; ++k){
+      setTimeout(() =>{
+        textHtml.textContent += text[k]
+        if(k==text.length-1) res();
+      }, 100*k)
+    }
+   
+  })
+
+  promise.then(() =>{
+    skipButton.style.pointerEvents='all';
+    console.log('ok')
+  })
+}
+
+const charactersSetup = {
+  'Netícia': {
+    'username': 'Netícia',
+    'src': './src/imgs/dialogue-nett.gif'
+  },
+  'Lelettering': {
+    'username': 'Lelettering',
+    'src': './src/imgs/dialogue-lelettering.gif'
+  },
+  'Letícia': {
+    'username': 'Letícia',
+    'src': './src/imgs/dialogue-lett.gif'
+  }
+}
+
+const dialogues = {
+  
+  0: {
+    'h4': charactersSetup.Lelettering.username,
+    'p': "AAAAAAAAAHHHHH!! O que aconteceu? Por que o mundo tá tão distorcido? Eu to com meedooo, muuuito medo...",
+    'src': charactersSetup.Lelettering.src
+  },
+  1: {
+    'h4': charactersSetup.Netícia.username,
+    'p': 'Cala a boquinha, garota, tava sonhando com coxinha e vc me acordou com essa voz de apito insuportável',
+    'src': charactersSetup.Netícia.src
+  },
+  2: {
+    'h4': charactersSetup.Netícia.username,
+    'p': 'Mas... parando pra pensar... você até que tem razão, to me sentindo meio estranha',
+    'src': charactersSetup.Netícia.src
+  }
+  
+}
+let currentDialogue = localStorage.getItem('currentDialogue') || 0;  
+
+console.log(localStorage.getItem('currentDialogue'))
+username.textContent = dialogues[currentDialogue].h4;
+paragraph.textContent = dialogues[currentDialogue].p
+writingAnimation(paragraph)
+
+character.src = dialogues[currentDialogue].src
+skipButton.addEventListener('click', () =>{
+  if(dialogues[currentDialogue+1]) currentDialogue++;
+  else return;
+  localStorage.setItem('currentDialogue', currentDialogue);
+  console.log(currentDialogue)
+  username.textContent = dialogues[currentDialogue].h4;
+  paragraph.textContent = dialogues[currentDialogue].p
+  writingAnimation(paragraph)
+  character.src = dialogues[currentDialogue].src
+
 })
